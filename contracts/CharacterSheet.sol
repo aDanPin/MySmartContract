087141c5453a7da03b770393427bf47c50bfdc66
aDanPin <drobrorobro@gmail.com>
2025-07-23 21:18:42 -0300

    Character init. store and history of changes.

--- contracts/CharacterSheet.sol
+++ contracts/CharacterSheet.sol
@@ -33,60 +33,59 @@ contract CharacterSheet {
 
     struct Sheet {
         bytes32 name;
+        AbilityScores abilityScores;
         RaceClass raceClass;
+    }
+
+    function diff(AbilityScores storage ls, AbilityScores calldata rs) internal {
+        ls.level = ls.level - rs.level;
+        ls.str = ls.str - rs.str;
+        ls.dex = ls.dex - rs.dex;
+        ls.con = ls.con - rs.con;
+        ls.intell = ls.intell - rs.intell;
+        ls.wis = ls.wis - rs.wis;
+        ls.cha = ls.cha - rs.cha;
+    }
+
+    function sum(AbilityScores storage ls, AbilityScores calldata rs) internal {
+        ls.level = ls.level + rs.level;
+        ls.str = ls.str + rs.str;
+        ls.dex = ls.dex + rs.dex;
+        ls.con = ls.con + rs.con;
+        ls.intell = ls.intell + rs.intell;
+        ls.wis = ls.wis + rs.wis;
+        ls.cha = ls.cha + rs.cha;
+    }
+
+    struct Character {
+        Sheet sheet;
+    }
+
+    struct Change {
+        uint256 timestamp;
         AbilityScores abilityScores;
     }
 
-    error differentSheetNames(bytes32 l, bytes32 r);
+    mapping (address => Change[]) changes;
+    mapping(address => Character) characters;
 
-    function diff(Sheet calldata ls, Sheet calldata rs) pure internal  returns (Sheet memory ans) {
-        if(ls.name == rs.name) {
-            revert differentSheetNames(ls.name, rs.name);
-        }
-
-        return Sheet(
-            ls.name,            
-            ls.raceClass,
-            AbilityScores(
-                ls.abilityScores.level - rs.abilityScores.level,
-                ls.abilityScores.str - rs.abilityScores.str,
-                ls.abilityScores.dex - rs.abilityScores.dex,
-                ls.abilityScores.con - rs.abilityScores.con,
-                ls.abilityScores.intell - rs.abilityScores.intell,
-                ls.abilityScores.wis - rs.abilityScores.wis,
-                ls.abilityScores.cha - rs.abilityScores.cha
-            )
-        );
+    function CommitChangeCharacter(
+                address owner,
+                AbilityScores calldata abilityScores 
+    ) internal {
+        sum(characters[owner].sheet.abilityScores, abilityScores);
+        changes[owner].push(Change(block.timestamp, abilityScores));
     }
 
-    function sum(Sheet calldata ls, Sheet calldata rs) pure internal  returns (Sheet memory ans) {
-        if(ls.name == rs.name) {
-            revert differentSheetNames(ls.name, rs.name);
-        }
-
-        return Sheet(
-            ls.name,            
-            ls.raceClass,
-            AbilityScores(
-                ls.abilityScores.level - rs.abilityScores.level,
-                ls.abilityScores.str - rs.abilityScores.str,
-                ls.abilityScores.dex - rs.abilityScores.dex,
-                ls.abilityScores.con - rs.abilityScores.con,
-                ls.abilityScores.intell - rs.abilityScores.intell,
-                ls.abilityScores.wis - rs.abilityScores.wis,
-                ls.abilityScores.cha - rs.abilityScores.cha
-            )
-        );
-    }
-
-    mapping(address => Sheet[]) public characters;
-
-    function getCharacter(address _owner, uint _index) public view returns (Sheet memory) {
-        require(_index < characters[_owner].length, "Character does not exist");
-        return characters[_owner][_index];
-    }
-
-    function getCharacterCount(address _owner) public view returns (uint) {
-        return characters[_owner].length;
+    function InitCharacter(
+                address owner,
+                bytes32 name,
+                RaceClass raceClass,
+                AbilityScores calldata abilityScores
+    ) internal {
+        Character storage character = characters[owner];
+        character.sheet.name = name;
+        character.sheet.raceClass = raceClass;
+        CommitChangeCharacter(owner, abilityScores);
     }
 }
